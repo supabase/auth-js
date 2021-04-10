229c8c7c1bddc26815e826f5bedbb5a32c9c78f5
Mark Burggraf <markb@mantisbible.com>
2021-04-10 16:50:08 -0700

    applied requested changes, copying the access_token before removing the session

--- src/GoTrueClient.ts
+++ src/GoTrueClient.ts
@@ -312,13 +312,14 @@ export default class GoTrueClient {
    * For server-side management, you can disable sessions by passing a JWT through to `auth.api.signOut(JWT: string)`
    */
   async signOut(): Promise<{ error: Error | null }> {
-    if (this.currentSession) {
-      const { error } = await this.api.signOut(this.currentSession.access_token)
+    const accessToken = this.currentSession?.access_token
+    this._removeSession()
+    this._notifyAllSubscribers('SIGNED_OUT')
+    if (accessToken) {
+      const { error } = await this.api.signOut(accessToken)
       if (error) {
         return { error }
       } else {
-        this._removeSession()
-        this._notifyAllSubscribers('SIGNED_OUT')
         return { error: null }
       }
     }
