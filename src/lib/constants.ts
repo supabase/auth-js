0875ffa84827d699481199d13902032b7eb7cc15
thorwebdev <thor@supabase.io>
2022-05-05 15:15:36 +0800

    chore: default expiry margin to 10s

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,7 +2,7 @@ import { version } from './version'
 export const GOTRUE_URL = 'http://localhost:9999'
 export const AUDIENCE = ''
 export const DEFAULT_HEADERS = { 'X-Client-Info': `gotrue-js/${version}` }
-export const EXPIRY_MARGIN = 60 // in seconds
+export const EXPIRY_MARGIN = 10 // in seconds
 export const NETWORK_FAILURE = 'NETWORK_FAILURE'
 export const NETWORK_FAILURE_RETRY_INTERVAL = 1 // in seconds
 export const STORAGE_KEY = 'supabase.auth.token'
