60074c7d69812e2ba4769668600315bcc9dd124f
Andrew Smith <a.smith@silentworks.co.uk>
2022-07-27 14:14:32 +0000

    Update examples to use .env file

--- example/next/utils/initSupabase.js
+++ example/next/utils/initSupabase.js
@@ -1,11 +1,13 @@
 import { GoTrueClient } from '@supabase/gotrue-js'
 
-const supabaseURL = 'http://localhost:9999'
+const supabaseURL = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnon = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
 const auth = new GoTrueClient({
-  url: `${supabaseURL}`,
+  url: `${supabaseURL}/auth/v1`,
   headers: {
     accept: 'json',
+    apikey: supabaseAnon,
   },
   // cookieOptions: { path: '/', name: 'meowncookie',  }, // Optional
 })
