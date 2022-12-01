d1037280cca09467d7ad2b264d75b65a6da0fdbb
Jason Creviston <jcreviston@pm.me>
2022-11-30 20:23:27 -0500

    add comment for regex

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -129,7 +129,9 @@ export class Deferred<T = any> {
 
 // Taken from: https://stackoverflow.com/questions/38552003/how-to-decode-jwt-token-in-javascript-without-using-a-library
 export function decodeJWTPayload(token: string) {
+  // Regex checks for base64url format
   const base64UrlRegex = /^([a-z0-9_-]{4})*($|[a-z0-9_-]{3}=?$|[a-z0-9_-]{2}(==)?$)$/i
+  
   const parts = token.split('.')
 
   if (parts.length !== 3) {
