53ebb23be5cca0d87169291e98f560b3f7b61ccf
Jonathan Picques <jonathan.picques@gmail.com>
2023-02-22 18:37:26 +0100

    Add keywords to listUsers to search users

--- src/GoTrueAdminApi.ts
+++ src/GoTrueAdminApi.ts
@@ -166,7 +166,7 @@ export default class GoTrueAdminApi {
    * @param params An object which supports `page` and `perPage` as numbers, to alter the paginated results.
    */
   async listUsers(
-    params?: PageParams
+    params?: PageParams & {keywords?: string}
   ): Promise<
     | { data: { users: User[]; aud: string } & Pagination; error: null }
     | { data: { users: [] }; error: AuthError }
@@ -179,6 +179,7 @@ export default class GoTrueAdminApi {
         query: {
           page: params?.page?.toString() ?? '',
           per_page: params?.perPage?.toString() ?? '',
+          keywords: params?.keywords ?? '',
         },
         xform: _noResolveJsonResponse,
       })
