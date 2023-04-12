d432e69e96be1253e6821e8a25d7315229c29883
Joel Lee <lee.yi.jie.joel@gmail.com>
2023-04-12 11:03:26 +0800

    Update GoTrueClient.ts

--- src/GoTrueClient.ts
+++ src/GoTrueClient.ts
@@ -1282,9 +1282,11 @@ export default class GoTrueClient {
       // finished and tests run endlessly. This can be prevented by calling
       // `unref()` on the returned object.
       ticker.unref()
+    // @ts-ignore
     } else if (Deno && typeof Deno !== "undefined" && typeof Deno.unrefTimer === 'function') {
       // similar like for NodeJS, but with the Deno API
       // https://deno.land/api@latest?unstable&s=Deno.unrefTimer
+      // @ts-ignore
       Deno.unrefTimer(ticker)
     }
 
