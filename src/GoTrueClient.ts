7a68cf7772c4a4cb4a5646017dcc6090c97faf22
bombillazo <hran@bombillazo.com>
2024-06-18 13:33:14 -0400

    chore: update file styling

--- src/GoTrueClient.ts
+++ src/GoTrueClient.ts
@@ -474,6 +474,7 @@ export default class GoTrueClient {
       if (isAuthError(error)) {
         return { data: { user: null, session: null }, error }
       }
+      
       throw error
     }
   }
